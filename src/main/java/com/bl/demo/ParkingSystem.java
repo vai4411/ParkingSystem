32842c7a4370d1f1e3ee59bd59c3108f79a55937
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-05-24 16:09:28 +0530

    ADDED: Check Car UnParked Or Not

--- src/main/java/com/bl/demo/ParkingSystem.java
+++ src/main/java/com/bl/demo/ParkingSystem.java
@@ -2,7 +2,21 @@ package com.bl.demo;
 
 public class ParkingSystem {
 
+    private Object vehicle;
+
     public boolean park(Object vehicle) {
+        if (this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if (vehicle == null) return false;
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/bl/demo/ParkingSystemTest.java
+++ src/test/java/com/bl/demo/ParkingSystemTest.java
@@ -1,14 +1,50 @@
 package com.bl.demo;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingSystemTest {
+    Object vehicle = null;
+    ParkingSystem parkingSystem = null;
+
+    @Before
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingSystem = new ParkingSystem();
+    }
 
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingSystem parkingSystem = new ParkingSystem();
         boolean isParked = parkingSystem.park(new Object());
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingSystem.park(vehicle);
+        boolean isParked = parkingSystem.park(new Object());
+        Assert.assertFalse(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingSystem.park(vehicle);
+        boolean isUnParked = parkingSystem.unPark(vehicle);
+        Assert.assertTrue(isUnParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenOtherVehicleUnParked_ShouldReturnFalse() {
+        parkingSystem.park(vehicle);
+        boolean isUnParked = parkingSystem.unPark(new Object());
+        Assert.assertFalse(isUnParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenNullVehicleUnParked_ShouldReturnFalse() {
+        parkingSystem.park(vehicle);
+        boolean isUnParked = parkingSystem.unPark(null);
+        Assert.assertFalse(isUnParked);
     }
 }
